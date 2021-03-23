91aa0641087a7f228528e847d55e23b7947c90d9
Alisher <advaster@yahoo.com>
2021-03-23 14:14:53 +0400

    Small changes

--- grafana-install-ubuntu.sh
+++ grafana-install-ubuntu.sh
@@ -4,18 +4,11 @@ sudo apt-get install -y apt-transport-https
 sudo apt-get install -y software-properties-common wget
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 
-
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 
 sudo apt update
-
 apt-cache policy grafana
 
-sudo apt install grafana
+sudo apt install grafana -y
 sudo systemctl start grafana-server
-sudo systemctl status grafana-server
 sudo systemctl enable grafana-server
-
-
-
-
