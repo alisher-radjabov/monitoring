e34f3bbb6a949baad289525332ae32f15c198bb0
Alisher <advaster@yahoo.com>
2021-03-23 14:04:33 +0400

    Install Grafana on Ubuntu

---
+++ grafana-install-ubuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get install -y apt-transport-https
+sudo apt-get install -y software-properties-common wget
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+
+
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+
+sudo apt update
+
+apt-cache policy grafana
+
+sudo apt install grafana
+sudo systemctl start grafana-server
+sudo systemctl status grafana-server
+sudo systemctl enable grafana-server
+
+
+
+
